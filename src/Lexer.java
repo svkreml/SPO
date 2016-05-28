b2deaaa10dee701ab3e558c12a9822f6306dd360
svkreml <svkreml@gmail.com>
2016-05-28 17:56:00 +0300

    /zero

--- src/Lexer.java
+++ src/Lexer.java
@@ -4,7 +4,9 @@ public class Lexer {
 
     private String str;
     private int spos = 0;
-
+    private int line =1;
+    private int Prev_pos =0;
+    private int pos;
     public Lexer(String str) {
         this.str = str;
     }
@@ -17,7 +19,11 @@ public class Lexer {
     private void skipSpaces() {
         while (spos < str.length()) {
             char ch = str.charAt(spos);
-            if (ch != ' ' && ch != '\n' && ch != '\t')
+            if(ch=='\r') {
+                line++;
+                Prev_pos=spos+2;// \r\n
+            }
+            if (ch != '\r'&&ch != ' ' && ch != '\n' && ch != '\t')
                 break;
             spos++;
         }
@@ -33,6 +39,7 @@ public class Lexer {
         ArrayList<Token> tokens = new ArrayList<Token>();
         //ArrayList<String> values = new ArrayList<String>();
         while (true) {
+            pos=spos;
             skipSpaces();
             if (spos >= str.length())
                 break;
@@ -42,20 +49,19 @@ public class Lexer {
                 while (spos < str.length() && isDigit(str.charAt(spos))) {
                     spos++;
                 }
-                tokens.add(new Token(Integer.parseInt(str.substring(x0,spos))));
+                tokens.add(new Token(Integer.parseInt(str.substring(x0,spos)), line, pos-Prev_pos));
 
             } else if (isLetter(ch)) {
                 int x0 = spos;
                 while (spos < str.length() && (isLetter(ch) || isDigit(ch))) {
                     ch = str.charAt(++spos);
                 }
-                tokens.add(new Token(str.substring(x0,spos)));
-                //values.add(str.substring(x0,spos));
+                tokens.add(new Token(str.substring(x0,spos), line, pos-Prev_pos));
             } else if ("+-*/()=;".indexOf(ch) >= 0) {
-                tokens.add(new Token(ch));
+                tokens.add(new Token(ch, line, spos-Prev_pos));
                 spos++;
             } else {
-                error("Unexpected character '" + ch + "'");
+                error("Unexpected character '" + ch + "'"+"in line:"+line+", Pos:"+(pos-Prev_pos));
             }
         }
         return tokens;
